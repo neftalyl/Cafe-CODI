32416c082dc63d9a482fa73b733a086cc0018a29
neftalyl <elchocoantio@gmail.com>
2015-01-29 11:07:16 -0500

    Reinicio Análisis de datos

---
+++ 2015-01-29 Semillero - Peso (A).R
@@ -0,0 +1,8 @@
+# Carlos N. Lozano - neftaly.lozano@gmail.com
+
+# PROYECTO CAFÉ - SEMILLERO
+
+### Datos y directorio -----
+setwd("~/UdeA/Bioma/Bacteria/Data Analysis/cafe")
+datos<-read.csv('2013-05-30 Semillero-Peso.csv',header=T)
+attach(datos)
